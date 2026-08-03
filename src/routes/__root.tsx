d92a33fe7535db590fa86112b7f6c60fcb36883b
Peers Plus <peersplushr@gmail.com>
2026-08-03 21:09:23 +0400

    GA

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -130,6 +130,18 @@ function RootShell({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VQ9XW9Z3FS"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-VQ9XW9Z3FS');
+            `,
+          }}
+        />
         <HeadContent />
       </head>
       <body>
